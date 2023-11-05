35498a5e74d9b555617261e6afa1941d70fa1ca9
qyupi <149958527+qyupi@users.noreply.github.com>
2023-11-05 19:46:40 +0300

    Add files via upload

--- Destroy.cs
+++ Destroy.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class Destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Приветствие мира
-        print("Hello, World!");
+        // Уничтожить объект
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("crickets");
+        
     }
 }
